c568cd1231af4821115ea7027f2974f1c28ff0d7
Jan-Michael Brummer <jan.brummer@tabos.org>
2023-01-16 10:44:03 +0100

    windows wip

--- src/tests/px-manager-helper.c
+++ src/tests/px-manager-helper.c
@@ -7,5 +7,5 @@ px_test_manager_new (void)
   /* g_autofree char *path = g_strdup_printf ("%s/../backend/modules", g_getenv ("G_TEST_BUILDDIR")); */
   g_autofree char *path = g_strdup_printf ("src/backend/plugins");
 
-  return g_object_new (PX_TYPE_MANAGER, "module-path", path, NULL);
+  return g_object_new (PX_TYPE_MANAGER, "plugins-dir", path, NULL);
 }
